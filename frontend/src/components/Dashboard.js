8c8d8b9d58a71009c41e9cb2a808c350ed33217f
Scott Syme <scottsyme@cs-wifi-162.cs.st-andrews.ac.uk>
2019-04-24 17:17:51 +0100

    lol

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -6,6 +6,17 @@ import ReleaseDate from './ReleaseDate';
 
 export default class Dashboard extends Component {
 
+    constructor (props) {
+        super(props);
+        this.state = {
+            top_genres: [],
+            top_release_dates: [],
+            top_track_features: [],
+            top_track_inst: [],
+            top_tracks_popularity: []
+        }
+    }
+
     componentDidMount(){
         fetch('/api/users/data', {// Use proxy
             method: 'POST',
@@ -17,6 +28,14 @@ export default class Dashboard extends Component {
             .then(result => result.json())// Parse the response
             .then((result) => {
                 console.log(result);
+                this.setState({
+                    top_genres: result.top_genres,
+                    top_release_dates: result.top_release_dates_count,
+                    top_track_features: result.top_track_feature,
+                    top_track_inst: result.top_track_inst,
+                    top_tracks_popularity: result.top_tracks_popularity
+                });
+                console.log(this.state.top_genres);
               }
             );
     }
@@ -24,7 +43,6 @@ export default class Dashboard extends Component {
     render() {
         return (
             <div>
-                Dashboard Component
                 <ReleaseDate></ReleaseDate>
             </div>
         );
